5aad69d6751be0021248de472e5928f8072c1d14
egg-tree <matthew.roman@student.allenisd.org>
2022-11-18 22:11:45 -0600

    polished code

---
+++ C101 Factorial [Swift]/main.swift
@@ -0,0 +1,27 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 20
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func calculate_Factorial(number : Int) -> Int {
+    //segmentation fault if the number reaches 21
+    precondition(n < 21, "Segmentation Fault")
+    
+    print("Calculating factorial(\(number))...")
+    if number <= 1 {
+        print("Terminal case, returning 1.")
+        return 1                                          
+    } else {        
+        let result = number * calculate_Factorial(number: number - 1)      
+        print("Non-terminal case, returning \(result).")
+        return result
+    }
+}
+
+print("The final result is: \(calculate_Factorial(number: n)).")
---
+++ C102 Fibonacci [Swift]/main.swift
@@ -0,0 +1,24 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 23
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func calculate_Fib(num: Int) -> Int {
+    print("Calculating fibonacci(\(num))...")
+    if num <= 2 {
+        print("Terminal case, returning 1.")
+        return 1
+    } else {
+        let fib = calculate_Fib(num: num - 1) + calculate_Fib(num: num - 2)
+        print("Non-terminal case, returning \(fib).")
+        return fib
+    }
+}
+
+print("The final result is: \(calculate_Fib(num: n)).")
